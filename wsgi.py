f7b2194302ff28dd276b47104aace683d23a4deb
AlonsoZeng <1040316595@qq.com>
2025-07-02 14:50:37 +0800

    1.0.9
    增加pythonanywhere配置项

---
+++ wsgi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+PythonAnywhere WSGI 配置文件
+"""
+
+import sys
+import os
+from pathlib import Path
+
+# 添加项目路径到 Python 路径
+project_home = '/home/zencold/news_on_kline'  # 替换为你的用户名
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# 设置环境变量
+os.environ['FLASK_ENV'] = 'production'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# 导入 Flask 应用
+from app import app as application
+
+if __name__ == "__main__":
+    application.run()
